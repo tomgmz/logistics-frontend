2a138fc2400d0fc05ff4373b3ea84f067e4c1e89
Tom <tomervingmz@gmail.com>
2026-03-28 02:38:21 +0800

    change next config .ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+const API_URL = process.env.API_URL ?? process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:4000'
+
+console.log('[next.config] API_URL:', API_URL)
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -10,7 +14,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL}/:path*`,
+        destination: `${API_URL}/api/:path*`,
       },
     ]
   },
