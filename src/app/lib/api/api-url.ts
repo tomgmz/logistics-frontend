925e13df26b8d87d6e5dda961195ba819b47508b
Tom <tomervingmz@gmail.com>
2026-03-29 18:07:32 +0800

    api url getter

--- src/app/lib/api/api-url.ts
+++ src/app/lib/api/api-url.ts
@@ -1,4 +1,4 @@
 export function getApiUrl(): string {
-  if (process.env.NODE_ENV === 'production') return '/proxy'
+  if (process.env.NODE_ENV === 'production') return '/api'
   return process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api'
 }
