2479bed99a5b8168cc66d9665d3ed98d7aad075c
Tom <tomervingmz@gmail.com>
2026-04-06 02:46:46 +0800

    rehydrate

--- src/components/AuthRehydrator.tsx
+++ src/components/AuthRehydrator.tsx
@@ -5,16 +5,14 @@ import { getMe } from '@/app/lib/api/auth.api'
 import { useAuthStore } from '@/app/lib/store/auth.store'
 
 export default function AuthRehydrator() {
-  const setUser   = useAuthStore((s) => s.setUser)
-  const clearUser = useAuthStore((s) => s.clearUser)
+  const setUser = useAuthStore((s) => s.setUser)
 
   useEffect(() => {
     getMe()
       .then(setUser)
       .catch(() => {
-        clearUser()
       })
-  }, [setUser, clearUser])
+  }, [setUser])
 
   return null
 }
